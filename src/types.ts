0731d900d22530e05c6574c01b2447049d15a0fa
Fadel.abou <fadel.abou@mehriso.com>
2025-07-15 12:56:10 +0100

    feat: start http server implementation

--- src/types.ts
+++ src/types.ts
@@ -19,3 +19,21 @@ export type Server = {
   host: string;
   port: number;
 };
+
+export type HTTPReq = {
+  method: string;
+  uri: Buffer;
+  version: string;
+  headers: Buffer[];
+};
+
+export type BodyReader = {
+  length: number;
+  read: () => Promise<Buffer>;
+};
+
+export type HTTPRes = {
+  code: number;
+  header: Buffer[];
+  body: BodyReader;
+};
